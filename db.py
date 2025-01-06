b84919e61895702e59c8dee3115214c0ed3d90ae
Jury Razumau <mail@razumau.net>
2025-01-06 15:39:17 +0100

    add sqlite3 adapters

--- db.py
+++ db.py
@@ -1,5 +1,6 @@
 import sqlite3
 import os
+import datetime
 
 from dotenv import load_dotenv
 
@@ -7,11 +8,44 @@ load_dotenv()
 
 DB_PATH = os.getenv('DB_PATH')
 
+def adapt_date_iso(val):
+    """Adapt datetime.date to ISO 8601 date."""
+    return val.isoformat()
+
+def adapt_datetime_iso(val):
+    """Adapt datetime.datetime to timezone-naive ISO 8601 date."""
+    return val.isoformat()
+
+def adapt_datetime_epoch(val):
+    """Adapt datetime.datetime to Unix timestamp."""
+    return int(val.timestamp())
+
+sqlite3.register_adapter(datetime.date, adapt_date_iso)
+sqlite3.register_adapter(datetime.datetime, adapt_datetime_iso)
+sqlite3.register_adapter(datetime.datetime, adapt_datetime_epoch)
+
+def convert_date(val):
+    """Convert ISO 8601 date to datetime.date object."""
+    return datetime.date.fromisoformat(val.decode())
+
+def convert_datetime(val):
+    """Convert ISO 8601 datetime to datetime.datetime object."""
+    return datetime.datetime.fromisoformat(val.decode())
+
+def convert_timestamp(val):
+    """Convert Unix epoch timestamp to datetime.datetime object."""
+    return datetime.datetime.fromtimestamp(int(val))
+
+sqlite3.register_converter("date", convert_date)
+sqlite3.register_converter("datetime", convert_datetime)
+sqlite3.register_converter("timestamp", convert_timestamp)
+
 def connection():
-    return sqlite3.connect(DB_PATH)
+    return sqlite3.connect(DB_PATH, detect_types=sqlite3.PARSE_DECLTYPES)
 
 def setup_database():
-    with sqlite3.connect(DB_PATH) as conn:
+    print("Creating tables if necessary...")
+    with connection() as conn:
         cursor = conn.cursor()
 
         cursor.execute('''
