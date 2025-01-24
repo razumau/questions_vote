91dbe11f3514951e2763790184ab802fb64c16b6
Jury Razumau <mail@razumau.net>
2025-01-24 17:38:19 +0100

    test creating a tournament

--- run_elo.py
+++ run_elo.py
@@ -1,16 +1,32 @@
 import time
-from elo import TournamentSystem
+from elo import Elo
+from models import Tournament, Question
 
-ts = TournamentSystem(list(range(10000)), std_dev_multiplier=1.5, initial_phase_matches=5)
+# ts = Elo(list(range(10000)), std_dev_multiplier=1.5, initial_phase_matches=5)
 
 start = time.time()
 
+question_ids = Question.question_ids_for_year(2022)
+print(len(question_ids))
+print(question_ids[0])
 
-for i in range(50000):
-    a, b = ts.select_pair()
-    ts.record_winner(winner_id=a, loser_id=b, timestamp=i)
 
-for i, item in enumerate(ts.get_top_items(250), 1):
-    print(f'{i}. {item[0]}: {item[1]:.2f} ({item[2]} matches)')
-print(ts.get_statistics())
-print(time.time() - start)
+Tournament.create_tournament(
+    question_ids=question_ids,
+    title="Test Tournament",
+    initial_k=64.0,
+    minimum_k=16.0,
+    std_dev_multiplier=1.5,
+    initial_phase_matches=5,
+    transition_phase_matches=10,
+    top_n=100,
+)
+
+# for i in range(50000):
+#     a, b = ts.select_pair()
+#     ts.record_winner(winner_id=a, loser_id=b, timestamp=i)
+#
+# for i, item in enumerate(ts.get_top_items(250), 1):
+#     print(f'{i}. {item[0]}: {item[1]:.2f} ({item[2]} matches, {item[3]} wins)')
+# print(ts.get_statistics())
+# print(time.time() - start)
