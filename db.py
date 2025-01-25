3cbfc010c4f60e674bbe2ae58af7825447c1e60d
Jury Razumau <mail@razumau.net>
2025-01-25 15:21:46 +0100

    ruff format

--- db.py
+++ db.py
@@ -6,49 +6,59 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_PATH = os.getenv('DB_PATH')
+DB_PATH = os.getenv("DB_PATH")
+
 
 def adapt_date_iso(val):
     """Adapt datetime.date to ISO 8601 date."""
     return val.isoformat()
 
+
 def adapt_datetime_iso(val):
     """Adapt datetime.datetime to timezone-naive ISO 8601 date."""
     return val.isoformat()
+
 
 def adapt_datetime_epoch(val):
     """Adapt datetime.datetime to Unix timestamp."""
     return int(val.timestamp())
 
+
 sqlite3.register_adapter(datetime.date, adapt_date_iso)
 sqlite3.register_adapter(datetime.datetime, adapt_datetime_iso)
 sqlite3.register_adapter(datetime.datetime, adapt_datetime_epoch)
+
 
 def convert_date(val):
     """Convert ISO 8601 date to datetime.date object."""
     return datetime.date.fromisoformat(val.decode())
 
+
 def convert_datetime(val):
     """Convert ISO 8601 datetime to datetime.datetime object."""
     return datetime.datetime.fromisoformat(val.decode())
+
 
 def convert_timestamp(val):
     """Convert Unix epoch timestamp to datetime.datetime object."""
     return datetime.datetime.fromtimestamp(int(val))
 
+
 sqlite3.register_converter("date", convert_date)
 sqlite3.register_converter("datetime", convert_datetime)
 sqlite3.register_converter("timestamp", convert_timestamp)
 
+
 def connection():
     return sqlite3.connect(DB_PATH, detect_types=sqlite3.PARSE_DECLTYPES)
+
 
 def setup_database():
     print("Creating tables if necessary...")
     with connection() as conn:
         cursor = conn.cursor()
 
-        cursor.execute('''
+        cursor.execute("""
             CREATE TABLE IF NOT EXISTS questions (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 gotquestions_id INTEGER NOT NULL,
@@ -65,10 +75,10 @@ def setup_database():
                 created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
                 updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
             )
-        ''')
+        """)
 
         # Create images table with foreign key to questions
-        cursor.execute('''
+        cursor.execute("""
             CREATE TABLE IF NOT EXISTS images (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 question_id INTEGER,
@@ -77,9 +87,9 @@ def setup_database():
                 mime_type TEXT,
                 FOREIGN KEY (question_id) REFERENCES questions (id)
             )
-        ''')
+        """)
 
-        cursor.execute('''
+        cursor.execute("""
             CREATE TABLE IF NOT EXISTS votes (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 user_id INTEGER NOT NULL,
@@ -90,9 +100,9 @@ def setup_database():
                 FOREIGN KEY (question1_id) REFERENCES questions (id),
                 FOREIGN KEY (question2_id) REFERENCES questions (id)
             )
-        ''')
+        """)
 
-        cursor.execute('''
+        cursor.execute("""
             CREATE TABLE IF NOT EXISTS packages (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     gotquestions_id INTEGER NOT NULL,
@@ -103,9 +113,9 @@ def setup_database():
                     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
                     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
                 )
-        ''')
+        """)
 
-        cursor.execute('''
+        cursor.execute("""
             CREATE TABLE IF NOT EXISTS tournaments (
                     id INTEGER PRIMARY KEY AUTOINCREMENT,
                     title TEXT NOT NULL,
@@ -116,11 +126,12 @@ def setup_database():
                     initial_phase_matches INTEGER DEFAULT 10,
                     transition_phase_matches INTEGER DEFAULT 20,
                     top_n INTEGER DEFAULT 100,
+                    questions_count INTEGER NOT NULL,
                     created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
                     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP
                 )
-                ''')
-        cursor.execute('''
+                """)
+        cursor.execute("""
             CREATE TABLE IF NOT EXISTS tournament_questions (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 tournament_id INTEGER NOT NULL,
@@ -129,29 +140,32 @@ def setup_database():
                 matches INTEGER DEFAULT 0,
                 wins INTEGER DEFAULT 0
             )
-        ''')
+        """)
         conn.commit()
     print("Tables created")
+
 
 def clean_database():
     with sqlite3.connect(DB_PATH) as conn:
         cursor = conn.cursor()
-        cursor.execute('''DELETE FROM packages''')
-        cursor.execute('''DELETE FROM images''')
-        cursor.execute('''DELETE FROM questions''')
-        cursor.execute('''DELETE FROM votes''')
+        cursor.execute("""DELETE FROM packages""")
+        cursor.execute("""DELETE FROM images""")
+        cursor.execute("""DELETE FROM questions""")
+        cursor.execute("""DELETE FROM votes""")
         conn.commit()
+
 
 def drop_tables():
     with sqlite3.connect(DB_PATH) as conn:
         cursor = conn.cursor()
-        cursor.execute('''DROP TABLE IF EXISTS packages''')
-        cursor.execute('''DROP TABLE IF EXISTS images''')
-        cursor.execute('''DROP TABLE IF EXISTS questions''')
-        cursor.execute('''DROP TABLE IF EXISTS votes''')
+        cursor.execute("""DROP TABLE IF EXISTS packages""")
+        cursor.execute("""DROP TABLE IF EXISTS images""")
+        cursor.execute("""DROP TABLE IF EXISTS questions""")
+        cursor.execute("""DROP TABLE IF EXISTS votes""")
         conn.commit()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     setup_database()
     # drop_tables()
     # clean_database()
